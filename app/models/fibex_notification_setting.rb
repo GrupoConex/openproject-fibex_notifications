a2b7f4bf37a6ceea1bba258ee539822b1c3384e3
Freddy Campos <marthianfred@gmail.com>
2026-07-02 19:30:00 -0400

    fix: remove encrypts (AR encryption not configured in OpenProject)

--- app/models/fibex_notification_setting.rb
+++ app/models/fibex_notification_setting.rb
@@ -3,6 +3,4 @@ class FibexNotificationSetting < ApplicationRecord
   validates :keycloak_token_endpoint, presence: true, url: true
   validates :keycloak_client_id, presence: true
   validates :keycloak_client_secret, presence: true
-
-  encrypts :keycloak_client_secret
 end
